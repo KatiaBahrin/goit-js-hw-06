339ae38d893994b4060f7e28c3db6855068cd6d7
Katia Bahrin <katia.b1101@gmail.com>
2023-09-28 15:32:59 +0200

    add task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+const fragment = document.createDocumentFragment(); // Фрагмент для оптимізації додавання елементів
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement('li'); // Новий <li> елемент
+  listItem.textContent = ingredient; // Текстовий вміст з масиву
+  listItem.classList.add('item'); // Додаємо клас "item" до <li>
+  fragment.appendChild(listItem); // Додаємо <li> до фрагмента
+});
+
+ingredientsList.appendChild(fragment);І
